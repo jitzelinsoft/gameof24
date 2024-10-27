0570a95853fbfea97f19a001f16cf22d03ef48bf
JHuangCode <j.s.huang@student.rug.nl>
2024-10-27 14:13:03 +0100

    First working concept

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int Calculate(int a, int b, int operator) {
-    char operators[4] = {'+', '-', '*', "/"};
-    switch (operator) {
+int calculate(int a, int b, int operator) {
+    char operators[4] = {'+', '-', '*', '/'};
+    switch (operators[operator]) {
         case '+':
             a = a + b;
             break;
@@ -11,8 +11,13 @@ int Calculate(int a, int b, int operator) {
             break;
         case '*':
             a = a * b;
+            break;
         case '/':
-            a = a / b;
+            if (b != 0 && a % b == 0) {
+                a = a / b;
+            } else {
+                a = 100000000000;  // Just to make the thing not 24
+            }
             break;
 
         default:
@@ -23,6 +28,7 @@ int Calculate(int a, int b, int operator) {
 }
 
 int main(int argc, char *argv[]) {
+    char operators[4] = {'+', '-', '*', '/'};
     /* Inputs into array */
     int array[4];
     for (int i = 0; i < 4; i++) {
@@ -31,27 +37,36 @@ int main(int argc, char *argv[]) {
 
     /* Outputs all possible combinations for a b c d */
     int a, b, c, d;
-    for (int i = 0; i < 4; i++) {
-        a = array[i];
-        for (int i = 0; i < 4; i++) {
-            b = array[i];
-            for (int i = 0; i < 4; i++) {
-                c = array[i];
-                for (int i = 0; i < 4; i++) {
-                    d = array[i];
-                    if (a != b && a != c && a != d && b != a && b != c && b != d && c != a && c != b && c != d && d != a && d != b && d != c) {  // Non optimized if statement
-                        printf("%d %d %d %d\n", a, b, c, d);
+    for (int i1 = 0; i1 < 4; i1++) {
+        a = array[i1];
+        for (int i2 = 0; i2 < 4; i2++) {
+            b = array[i2];
+            for (int i3 = 0; i3 < 4; i3++) {
+                c = array[i3];
+                for (int i4 = 0; i4 < 4; i4++) {
+                    d = array[i4];
+                    if (i1 != i2 && i1 != i3 && i1 != i4 && i2 != i1 && i2 != i3 && i2 != i4 && i3 != i1 && i3 != i2 && i3 != i4 && i4 != i1 && i4 != i2 && i4 != i3) {  // Non optimized if statement
+                        // printf("%d %d %d %d\n", a, b, c, d);
+                        for (int i = 0; i < 4; i++) {          // i = First operator
+                            for (int z = 0; z < 4; z++) {      // z = Second operator
+                                for (int y = 0; y < 4; y++) {  // y = last operator
+                                    // printf("(%d %c %d) %c (%d %c %d) = %d = %d %c %d\n", a, operators[i], b, operators[z], c, operators[y], d, calculate(calculate(a, b, i), calculate(c, d, y), z), calculate(a, b, i), operators[z], calculate(c, d, y));
+                                    if (calculate(calculate(a, b, i), calculate(c, d, y), z) == 24) {  // (a.b).(c.d) '.' = operator
+                                        printf("((%d %c %d) %c (%d %c %d))\n", a, operators[i], b, operators[z], c, operators[y], d);
+                                        return 0;
+                                    } else if (calculate(calculate(a, calculate(b, c, z), i), d, y) == 24) {  // (a.(b.c)).d
+                                        printf("((%d %c (%d %c %d) %c %d)\n", a, operators[i], b, operators[z], c, operators[y], d);
+                                        return 0;
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
     }
-    for (int i = 0; i < 4; i++) {
-        for (int z = 0; z < 4; z++) {
-            for (int y = 0; y < 4; y++) {
-            }
-        }
-    }
+
     printf("No solution\n");
     return 0;
 }
