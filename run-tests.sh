7dd42840883fe1392feb69ede41180bc5e56a96a
Jitze Lindeboom <jitze@linsoft.nl>
2024-11-02 15:03:34 +0100

    feat: rigorous testing

--- run-tests.sh
+++ run-tests.sh
@@ -4,5 +4,20 @@ make clean
 make build
 
 while read line; do
-	./bin/gameof24 <<< "$line"
-done<./tests.in
+	e=$(./bin/gameof24 <<< "$line" | tail -1)
+	n=$(echo "$line" | tr -d ' ')
+	printf "$n: "
+	if [ "$e" = "No solution" ]; then
+		echo "$e"
+	else
+		r=$(echo "scale=5; $e" | bc)
+		echo "$e = $r"
+		# Define a tolerance for the comparison
+		diff=$(echo "scale=5; $r - 24" | bc -l)
+		if (( $(echo "$diff < -0.1 || $diff > 0.1" | bc -l) )); then
+			echo "Test failed!"
+			exit 1
+		fi
+	fi
+done < ./tests.in
+
